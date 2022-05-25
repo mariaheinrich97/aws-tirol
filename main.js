258faa0d7367189dfaa9bc2b66664a3a664c2e7e
Maria <maria.heinrich@student.uibk.ac.at>
2022-05-25 18:24:10 +0200

    console.log auskommentiert

--- main.js
+++ main.js
@@ -57,9 +57,9 @@ overlays.stations.addTo(map);
 
 // Farben nach Wert und Schwellen ermitteln
 let getColor = function (value, ramp) {
-    console.log(value, ramp);
+    //console.log(value, ramp);
     for (let rule of ramp) {
-        console.log(rule);
+        //console.log(rule);
         if (value >= rule.min && value < rule.max) {
             return rule.color;
         }
